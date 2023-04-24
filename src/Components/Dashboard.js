262f7583d9fe920568143b80fd94b347e1d6858f
hadiayser <111729440+hadiayser@users.noreply.github.com>
2023-04-24 14:42:06 +0200

    fetcha api i console

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,5 +1,16 @@
-export default function Dashboard(){
-    return(
-        <h1>Games</h1>
-    )
+import React, {useEffect} from "react";
+
+export default function Dashboard() {
+  useEffect(() => {
+    const hentSpill = async () => {
+      const response = await fetch(`https://api.rawg.io/api/games?key=d756e11d41c9402e9f5ee9be11373a5f`);
+      const data = await response.json();
+      console.log(data); 
+    };
+    hentSpill();
+  }, []);
+
+  return (
+   <></>
+  );
 }
