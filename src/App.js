4a7436044ef79ec6195818723083a14a7556189e
hadiayser <111729440+hadiayser@users.noreply.github.com>
2023-04-24 15:43:34 +0200

    tester rundt med api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import '../src/css/main.css'
 import Dashboard from './Components/Dashboard';
 
 function App() {
--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,16 +1,27 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 
 export default function Dashboard() {
+const [games, setGames] = useState([]);
+
   useEffect(() => {
     const hentSpill = async () => {
       const response = await fetch(`https://api.rawg.io/api/games?key=d756e11d41c9402e9f5ee9be11373a5f`);
       const data = await response.json();
-      console.log(data); 
+      const test1= data.results; 
+      setGames(test1)
+      console.log(test1)
     };
     hentSpill();
   }, []);
 
   return (
-   <></>
+   <>
+   {games.map(game =>(
+    <div key={game.id}>
+        <p>{game.name}</p>
+        <img src={game.background_image}></img>
+    </div>
+   ))}
+   </>
   );
 }
