8630bceaa214eb7bbe77e3f18e1cd45b4137b014
FinnJakob <finnjakobe@gmail.com>
2023-05-07 14:36:40 +0200

    EndringerGCArds

--- src/Components/GameCard.js
+++ src/Components/GameCard.js
@@ -1,5 +1,6 @@
+import { useParams, Link } from "react-router-dom";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 export default function GameCard() {
   const { slug } = useParams();
@@ -7,7 +8,7 @@ export default function GameCard() {
 
   useEffect(() => {
     const fetchGame = async () => {
-      const response = await fetch(`https://api.rawg.io/api/games/${slug}?key=d756e11d41c9402e9f5ee9be11373a5f`);
+      const response = await fetch(https://api.rawg.io/api/games/${slug}?key=d756e11d41c9402e9f5ee9be11373a5f);
       const data = await response.json();
       setGame(data);
     };
@@ -18,6 +19,9 @@ export default function GameCard() {
     <div>
       <h1>{game?.name}</h1>
       <img src={game?.background_image} alt={game?.name} />
+      <Link to={/gamepage/${slug}}>
+        <button>Spillside</button>
+      </Link>
       <p>{game?.description_raw}</p>
     </div>
   );
--- src/Components/GamePage.js
+++ src/Components/GamePage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+
 export default function GamePage() {
   const { slug } = useParams();
   const [game, setGame] = useState(null);
