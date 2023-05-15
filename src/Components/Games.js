33aa9c1b69b5d0bde99de7fc53c565e7be8b4447
hadiayser <111729440+hadiayser@users.noreply.github.com>
2023-05-15 19:51:33 +0200

    Gjør koden mer lesbar og gjenbrukbar

---
+++ src/Components/Games.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+export default function Games({game}){
+    return(
+        <div key={game.id}>
+        <p>{game.name}</p>
+        <p>{game.genres.map((genre) => genre.name).join(", ")}</p>
+        <Link to={`/game/${game.slug}`}>
+            <img src={game.background_image}></img>
+            </Link>
+            <Link to="/gameshop">
+        <button>Visit shop</button>
+      </Link>
+    </div> 
+    )
+}
