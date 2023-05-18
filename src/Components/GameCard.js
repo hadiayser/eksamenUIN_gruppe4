be91c3875977086a01a0813e968ecff7c5f70be6
marielve <mariel99@live.no>
2023-05-18 15:27:00 +0200

    fikset gamecard og gameshop component

--- src/Components/GameCard.js
+++ src/Components/GameCard.js
@@ -1,28 +1,21 @@
-import React, { useEffect, useState } from "react";
-import { useParams, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-export default function GameCard() {
-  const { slug } = useParams();
-  const [game, setGame] = useState(null);
-
-  useEffect(() => {
-    const fetchGame = async () => {
-      const response = await fetch(`https://api.rawg.io/api/games/${slug}?key=d756e11d41c9402e9f5ee9be11373a5f`);
-      const data = await response.json();
-      setGame(data);
-    };
-    fetchGame();
-  }, [slug]);
-
+const GameCard = ({ game, addToFavorites, showVisitShop  }) => {
   return (
-    <div className="game-card">
-    <h1 className="gamecard-name">{game?.name}</h1>
-    <p>{game?.genres.map((genre) => genre.name).join(", ")}</p>
-    <img className="gamecard-image" src={game?.background_image} alt={game?.name} />
-    <Link to={`/gamepage/${slug}`}>
-        <button>Spillside</button>
+    <div className="game-shop" key={game.id}>
+      <p className="game-name">{game.name}</p>
+      <p>{game.genres.map((genre) => genre.name).join(", ")}</p>
+      <Link to={`/game/${game.slug}`}>
+        <img className="game-image" src={game.background_image} alt={game.name} />
       </Link>
-    <p className= "game-description">{game?.description_raw}</p>
+      {showVisitShop &&(
+      <Link to="/gameshop">
+        <button>Visit shop</button>
+      </Link>
+        )}
+      <button onClick={() => addToFavorites(game)}>Add to favorites</button>
     </div>
   );
-}
+};
+
+export default GameCard;
--- src/Components/Gameshop.js
+++ src/Components/Gameshop.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import { useHentSpill } from "./HentSpill";
+import { Link } from "react-router-dom";
 export default function Gameshop(){
 
     const url = "https://api.rawg.io/api/games?key=d756e11d41c9402e9f5ee9be11373a5f&ordering=released&page_size=10";
@@ -11,13 +12,13 @@ export default function Gameshop(){
     <div className="game-shop" key={game.id}>
         <p className="game-name">{game.name}</p>
         <p>{game.genres.map((genre) => genre.name).join(", ")}</p>
-        <img className="game-image" src={game.background_image}></img>
+        <Link to={`/game/${game.slug}`}>
+          <img className="game-image" src={game.background_image} alt={game.name} />
+          </Link>
     </div>
    ))}
    </>
   );
 }
-
-
 
 
